a6f41b03eea95c130adf0492e4ec76b6d965fc22
Pedro Martins <pedro.rufino.martins@gmail.com>
2025-08-18 12:39:15 -0300

    exploring data

--- exploratory_biomass_palm.R
+++ exploratory_biomass_palm.R
@@ -8,6 +8,34 @@ Biomass_palms_archontophoenix$DAP_cm <- c(Biomass_palms_archontophoenix$DAP_mm/1
 #View (Biomass_palms_archontophoenix)
 #Biomass_palms_archontophoenix$DAP_cm < 7
 
-plot (biomassa_fresca_g~DAP_cm, data = Biomass_palms_archontophoenix, log = "xy")
-plot (biomassa_fresca_g~altura_cm, data = Biomass_palms_archontophoenix, log = "xy")
-plot (DAP_cm~altura_cm, data = Biomass_palms_archontophoenix, log = "xy")
+bio_dap <- lm (biomassa_fresca_g~DAP_cm, data =
+                 Biomass_palms_archontophoenix)
+bio_dap_2 <- lm (biomassa_fresca_g~I (DAP_cm*DAP_cm^2), data =
+                 Biomass_palms_archontophoenix)
+
+summary (anova (bio_dap,bio_dap_2))
+
+summary(bio_dap)
+plot (biomassa_fresca_g~DAP_cm, data =
+        Biomass_palms_archontophoenix)
+abline (bio_dap_2)
+
+bio_alt <- lm (biomassa_fresca_g~altura_cm, data =
+      Biomass_palms_archontophoenix)
+summary(bio_alt)
+plot (biomassa_fresca_g~altura_cm, data =
+        Biomass_palms_archontophoenix)
+abline (bio_alt)
+abline (h=mean(Biomass_palms_archontophoenix$biomassa_fresca_g,
+             na.rm = TRUE))
+abline (v=mean(Biomass_palms_archontophoenix$altura_cm,
+               na.rm = TRUE))
+
+
+alt_dap <- lm (DAP_cm~altura_cm, data =
+                 Biomass_palms_archontophoenix)
+summary(alt_dap)
+plot (DAP_cm~altura_cm, data =
+        Biomass_palms_archontophoenix)
+abline (alt_dap)
+
